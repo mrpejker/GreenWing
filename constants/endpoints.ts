efe63eb375e36bfd779c1873447f0ef3f11e2c91
Vasily Kharlamov <caesai@yandex.ru>
2022-03-21 15:25:30 +0300

    New Contract Endpoint

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,5 +1,5 @@
 export enum Endpoints {
   TESTNET_CONTRACT_NAME = 'vself-staging.ilerik.testnet',
   TESTNET_RPC_ENDPOINT_URI = 'https://rpc.testnet.near.org',
-  TESTNET_CONTRACT_URI = 'beta_v9.sergantche.testnet',
+  TESTNET_CONTRACT_URI = 'beta_v10.sergantche.testnet',
 }
