682365e03ddecf9a23e1e2bea670b7dd7cf5d172
mrpejker <tatiana.yakushkina@gmail.com>
2023-01-07 18:08:33 +0400

    GreenWing frontend upd 4

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -17,7 +17,7 @@ const AboutPage: NextPage = () => {
     <div className="bg-[#fff]">
       <div className="flex flex-col items-center px-[20px]">
         <div className="flex flex-col md:flex-row max-w-[1240px] bg-[#343434] p-[30px] md:p-[75px] mt-[155px] rounded-xl overflow-hidden">
-          <div className="md:w-2/3">
+          <div className="md:w-4/5">
             <h1 className="font-grotesk uppercase text-[#fff] sm:text-[32px] text-[25px]">
             GreenWing is a digital solution that provides traceability for sustainable aviation fuels (SAF) certificates using blockchain technology.
             </h1>
@@ -32,10 +32,10 @@ const AboutPage: NextPage = () => {
 
 
         <div className="flex flex-col sm:flex-row max-w-[1240px] bg-[url(/mission_bg.png)] bg-cover bg-no-repeat rounded-xl mt-[100px] mb-[40px] pt-[40px] px-[80px] font-inter">
-          <div className="flex flex-col w-full md:w-2/3">
+          <div className="flex flex-col w-full md:w-4/5">
             <h2 className="font-drukMedium text-[#fff] text-[25px] uppercase">Mission</h2>
             <p className="font-interBold text-[#fff] text-[20px] my-[35px]">What is GreenWing?</p>
-            <p className="text-[#fff] text-[20px]">
+            <p className="text-[#fff] text-[40px]">
             The creation of a national or global SAF platform could expand the typical bilateral relationship 
             between producers and buyers, providing better information and competitive prices to end customers. 
             Standardization of credentials and automation of the supply chain management allows for more independent 
@@ -50,12 +50,6 @@ const AboutPage: NextPage = () => {
             data-aos="slide-up"
             data-aos-delay="600"
           >
-            <div className="flex flex-col items-center">
-              <div className="animate-ninja_bounce mb-[-40px] relative">
-                <img className="" src="/lnd_nj.png" alt="ninja" height={180} width={145} />
-              </div>
-              <img className="-top-[50%] animate-pulse" height={370} width={109} src="/lnd_tail.png" alt="ninja" />
-            </div>
           </div>
         </div>
       </div>
