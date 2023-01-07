71cd153a2b6831a8ed2ff391dd422210b713b34a
ilerik <user@LAPTOP-HJKKDMLP.localdomain>
2023-01-07 14:52:35 +0300

    fix

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -6,6 +6,13 @@ export enum Endpoints {
   TESTNET_CONTRACT_URI = 'events.greenwingadmin.testnet',
   MAINNET_CONTRACT_URI = 'v3.event.vself.near',
 
+  // Linkdrop Contracts
+  LINKDROP_TESTNET_CONTRACT_URI = 'linkdrop_v2.sergantche.testnet',
+
+  // Deposits Contracts
+  DEPOSITS_TESTNET_CONTRACT_URI = 'dev-1666966310314-54040938400138',
+  DEPOSITS_MAINNET_CONTRACT_URI = 'dev-1666696088646-92999275504703',
+
   // Near social
   SOCIAL_CONTRACT_URI = 'v1.social08.testnet',
 }
